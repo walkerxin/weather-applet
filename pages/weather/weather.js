d483032b30345ce87ff62771cc495bbb9cbfe216
walkerxin <1464193658@qq.com>
2019-07-07 23:19:37 +0800

    暂调用伪天气API并展示

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,15 +1,60 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const colorMap = {
+  'sunny': '#c4efff',
+  'cloudy': '#daeff7',
+  'overcast': '#c4ced2',
+  'lightrain': '#b6d6e2',
+  'heavyrain': '#c3ccd0',
+  'snow': '#99e3ff'
+}
+
 Page({
   data: {
-    temperature: 12,
-    desc: '晴天'
+    temperature: '12',
+    desc: '晴天',
+    weatherBg: 'sunny-bg'
   },
   onLoad() {
     this.getWeather()
   },
   onPullDownRefresh() {
-    this.getWeather()
+    this.getWeather(() => {
+      wx.stopPullDownRefresh()
+    })
   },
-  getWeather() {
+  getWeather(callback) {
     
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '深圳'
+      },
+      success: rs => {
+
+        let result = rs.data.result
+        this.setData({
+          temperature: result.now.temp,
+          desc: weatherMap[result.now.weather],
+          weatherBg: result.now.weather + '-bg'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: colorMap[result.now.weather],
+        })
+
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+
   }
 })
