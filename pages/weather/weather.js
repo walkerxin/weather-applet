d7adeda7d5100482cfa1fbbe2e1586a97f5722f4
walkerxin <1464193658@qq.com>
2019-07-07 11:22:10 +0800

    导航栏背景色 & 开启下拉刷新 & 使用{{mustach}}绑定

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,1 +1,15 @@
-Page({})
+Page({
+  data: {
+    temperature: 12,
+    desc: '晴天'
+  },
+  onLoad() {
+    this.getWeather()
+  },
+  onPullDownRefresh() {
+    this.getWeather()
+  },
+  getWeather() {
+    
+  }
+})
